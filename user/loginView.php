22bb3d594a93b8cdae60fc2b76d76a378a40f82d
lxs0401 <fengod0401@gmail.com>
2018-08-26 10:23:53 +0800

    432

--- user/loginView.php
+++ user/loginView.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php
+session_start();
+if(isset($_SESSION['user']))
+{
+    if($_SESSION['type'] == '2'){
+        header("location:/admin/index.php");
+    }
+}
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
