fe4174f337851e8c6fc1a30873606c72ddf89c71
lxs0401 <fengod0401@gmail.com>
2018-09-10 15:14:37 +0800

    5

--- install.php
+++ install.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
+<?php
+$con = mysql_connect("localhost","root","wslzd9877");
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("user", $con);
+if(isset($_GET['install']))
+{
+    mysql_query("CREATE TABLE `delfiles` (  `orderId` char(32) NOT NULL,  `time` char(20) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+    mysql_query("CREATE TABLE `fileinfo` (  `orderId` char(32) NOT NULL,  `filePath` varchar(100) NOT NULL,  `color` char(1) NOT NULL DEFAULT '1',  `num` int(11) NOT NULL DEFAULT '1',  `paperType` int(11) NOT NULL DEFAULT '16',  `paperWay` char(1) CHARACTER SET utf32 NOT NULL DEFAULT '1',  `filename` varchar(1000) NOT NULL,  `otherInfo` varchar(500) NOT NULL DEFAULT '') ENGINE=MyISAM DEFAULT CHARSET=utf8");
+    mysql_query("CREATE TABLE `orderids` (  `orderId` char(32) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+    mysql_query("CREATE TABLE `orderinfo` (  `orderId` char(32) NOT NULL,  `consumer` char(15) NOT NULL,  `business` char(15) NOT NULL,  `deadline` varchar(13) NOT NULL,  `orderState` int(11) NOT NULL DEFAULT '1',  `orderInfo` varchar(400) NOT NULL DEFAULT '',  `exCode` char(6) NOT NULL,  `deleted` char(2) NOT NULL DEFAULT 'nn',  PRIMARY KEY (`orderId`)) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+    mysql_query("CREATE TABLE `printerinfo` (  `username` char(15) NOT NULL,  `color` char(1) NOT NULL DEFAULT '1',  `paperType` int(11) NOT NULL DEFAULT '16',  PRIMARY KEY (`username`)) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+    mysql_query("CREATE TABLE `user` (  `username` char(15) NOT NULL,  `password` varchar(16) NOT NULL,  `type` char(1) NOT NULL,  `province` char(50) NOT NULL,  `city` char(50) NOT NULL,  `area` char(50) NOT NULL,  `other` varchar(400) NOT NULL,  `lo` varchar(15) NOT NULL,  `la` varchar(15) NOT NULL,  `state` char(1) NOT NULL,  `phone` char(11) NOT NULL,  PRIMARY KEY (`username`)) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+}
+
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta name="viewport" content="width=device-width"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="robots" content="noindex,nofollow" />
 	<title>云打印安装文件</title>
+    <link rel="stylesheet" type="text/css" href="/css/buttons.css" />
 	
 </head>
 <body class="bgrd">
@@ -13,40 +32,7 @@
 <br>校园云打印 BOS团队
 </center>
 <div class="main">
-<form method="post" action="setup-config.php?step=2">
-<center><h1 class="screen-reader-text">设置您的数据库连接</h1>
-	
-	<table class="form-table">
-		<tr>
-			<th scope="row"><label for="dbname">数据库名</label></th>
-			<td><input name="dbname" id="dbname" type="text" size="25" value="database" /></td>
-			<td>安装在哪一个数据库？</td>
-		</tr>
-		<tr>
-			<th scope="row"><label for="uname">用户名</label></th>
-			<td><input name="uname" id="uname" type="text" size="25" value="用户名" /></td>
-			<td>您的数据库用户名。</td>
-		</tr>
-		<tr>
-			<th scope="row"><label for="pwd">密码</label></th>
-			<td><input name="pwd" id="pwd" type="text" size="25" value="密码" autocomplete="off" /></td>
-			<td>您的数据库密码。</td>
-		</tr>
-		<tr>
-			<th scope="row"><label for="dbhost">数据库主机</label></th>
-			<td><input name="dbhost" id="dbhost" type="text" size="25" value="localhost" /></td>
-			<td>默认为<code>localhost</code>，其他改为自定义的。</td>
-		</tr>
-		<tr>
-			<th scope="row"><label for="prefix">表前缀</label></th>
-			<td><input name="prefix" id="prefix" type="text" value="sdut1_" size="25" /></td>
-			<td>请输入表前缀</td>
-		</tr>
-	</table>
-		<input type="hidden" name="language" value="zh_CN" />
-	<p class="step"><input name="submit" type="submit" value="提交" class="button button-large" /></p>
-	</center>
-</form>
+    <a class="button button-block button-rounded button-large" href="install.php?install=">Begin Install</a>
 </div>
 <script type='text/javascript' src='http://htmlblog.sdut1.com/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
 <script type='text/javascript' src='http://htmlblog.sdut1.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
--- user.sql
+++ user.sql
@@ -1,68 +1,23 @@
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS 'delfiles' (  'orderId' char(32) NOT NULL,  'time' char(20) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8
 
 
 
-
-CREATE TABLE IF NOT EXISTS `delfiles` (
-  `orderId` char(32) NOT NULL,
-  `time` char(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS 'fileinfo' (  'orderId' char(32) NOT NULL,  'filePath' varchar(100) NOT NULL,  'color' char(1) NOT NULL DEFAULT '1',  'num' int(11) NOT NULL DEFAULT '1',  'paperType' int(11) NOT NULL DEFAULT '16',  'paperWay' char(1) CHARACTER SET utf32 NOT NULL DEFAULT '1',  'filename' varchar(1000) NOT NULL,  'otherInfo' varchar(500) NOT NULL DEFAULT '') ENGINE=MyISAM DEFAULT CHARSET=utf8
 
 
-CREATE TABLE IF NOT EXISTS `fileinfo` (
-  `orderId` char(32) NOT NULL,
-  `filePath` varchar(100) NOT NULL,
-  `color` char(1) NOT NULL DEFAULT '1',
-  `num` int(11) NOT NULL DEFAULT '1',
-  `paperType` int(11) NOT NULL DEFAULT '16',
-  `filename` varchar(1000) NOT NULL,
-  `otherInfo` varchar(500) NOT NULL DEFAULT ''
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS 'orderids' (  'orderId' char(32) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8
 
 
-CREATE TABLE IF NOT EXISTS `orderids` (
-  `orderId` char(32) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS 'orderinfo' (  'orderId' char(32) NOT NULL,  'consumer' char(15) NOT NULL,  'business' char(15) NOT NULL,  'deadline' varchar(13) NOT NULL,  'orderState' int(11) NOT NULL DEFAULT '1',  'orderInfo' varchar(400) NOT NULL DEFAULT '',  'exCode' char(6) NOT NULL,  'deleted' char(2) NOT NULL DEFAULT 'nn',  PRIMARY KEY ('orderId')) ENGINE=MyISAM DEFAULT CHARSET=utf8
 
 
-CREATE TABLE IF NOT EXISTS `orderinfo` (
-  `orderId` char(32) NOT NULL,
-  `consumer` char(15) NOT NULL,
-  `business` char(15) NOT NULL,
-  `deadline` varchar(13) NOT NULL,
-  `orderState` int(11) NOT NULL DEFAULT '1',
-  `orderInfo` varchar(400) NOT NULL DEFAULT '',
-  `exCode` char(6) NOT NULL,
-  `deleted` char(2) NOT NULL DEFAULT 'nn',
-  PRIMARY KEY (`orderId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE IF NOT EXISTS `printerinfo` (
-  `username` char(15) NOT NULL,
-  `color` char(1) NOT NULL DEFAULT '1',
-  `paperType` int(11) NOT NULL DEFAULT '16',
-  PRIMARY KEY (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS 'printerinfo' (  'username' char(15) NOT NULL,  'color' char(1) NOT NULL DEFAULT '1',  'paperType' int(11) NOT NULL DEFAULT '16',  PRIMARY KEY ('username')) ENGINE=MyISAM DEFAULT CHARSET=utf8
 
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `username` char(15) NOT NULL,
-  `password` varchar(16) NOT NULL,
-  `type` char(1) NOT NULL,
-  `province` char(50) NOT NULL,
-  `city` char(50) NOT NULL,
-  `area` char(50) NOT NULL,
-  `other` varchar(400) NOT NULL,
-  `lo` varchar(15) NOT NULL,
-  `la` varchar(15) NOT NULL,
-  `state` char(1) NOT NULL,
-  `phone` char(11) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` (`username`, `password`, `type`, `province`, `city`, `area`, `other`, `lo`, `la`, `state`, `phone`) VALUES
-('lxs0401', 'wslzd9877', '1', '', '', '', '', '', '', '1', '13280672532'),
-('Lovers', 'wslzd9877', '2', '%u5C71%u4E1C%u7701', '%u9752%u5C9B%u5E02', '%u5E02%u5317%u533A', 'kfldkfls', '120.639978', '36.184754', '2', '13280672532');
+
+CREATE TABLE IF NOT EXISTS 'user' (  'username' char(15) NOT NULL,  'password' varchar(16) NOT NULL,  'type' char(1) NOT NULL,  'province' char(50) NOT NULL,  'city' char(50) NOT NULL,  'area' char(50) NOT NULL,  'other' varchar(400) NOT NULL,  'lo' varchar(15) NOT NULL,  'la' varchar(15) NOT NULL,  'state' char(1) NOT NULL,  'phone' char(11) NOT NULL,  PRIMARY KEY ('username')) ENGINE=MyISAM DEFAULT CHARSET=utf8
