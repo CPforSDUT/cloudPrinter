e19de10c0a43a9296dd535d97bcf88c25dd17de7
Luchen Zhao <da_kids@163.com>
2018-09-18 23:17:59 +0800

    4

--- getLocation.php
+++ getLocation.php
@@ -1,5 +1,3 @@
 <?php
-$province = $_POST['province'];
 $html2 = file_get_contents("http://api.map.baidu.com/location/ip?ak=04uLKfHLu2zT9eKoaSk2WsXC0ekF3aF3&coor=bd09ll");
-
 echo "var tpoint=" . $html2 .";\n";
