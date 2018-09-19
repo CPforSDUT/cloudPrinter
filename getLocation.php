c8fae51b420460cb560f0cc40e5aa5d46cc8b64c
lxs0401 <fengod0401@gmail.com>
2018-09-19 16:44:13 +0800

    6

--- getLocation.php
+++ getLocation.php
@@ -1,4 +1,4 @@
 <?php
-$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$ip = $_SERVER['REMOTE_ADDR'];
 $html2 = file_get_contents("http://api.map.baidu.com/location/ip?ip=$ip&ak=04uLKfHLu2zT9eKoaSk2WsXC0ekF3aF3&coor=bd09ll");
 echo "var tpoint=" . $html2 .";\n";
