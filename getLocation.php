d3b5a578926c46d79defe7366df3541869f7bafa
lxs0401 <fengod0401@gmail.com>
2018-09-19 16:40:40 +0800

    5

--- getLocation.php
+++ getLocation.php
@@ -1,3 +1,4 @@
 <?php
-$html2 = file_get_contents("http://api.map.baidu.com/location/ip?ak=04uLKfHLu2zT9eKoaSk2WsXC0ekF3aF3&coor=bd09ll");
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$html2 = file_get_contents("http://api.map.baidu.com/location/ip?ip=$ip&ak=04uLKfHLu2zT9eKoaSk2WsXC0ekF3aF3&coor=bd09ll");
 echo "var tpoint=" . $html2 .";\n";
