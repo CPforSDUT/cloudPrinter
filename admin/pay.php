7447fd89bacc53280d92e4d901d652c9e23099d5
lxs0401 <fengod0401@gmail.com>
2018-10-05 08:23:43 +0800

    1

--- admin/pay.php
+++ admin/pay.php
@@ -1,6 +1,27 @@
 ﻿<!doctype html>
 <?php
 session_start();
+function unescape($str) {
+    $ret = '';
+    $len = strlen ( $str );
+    for($i = 0; $i < $len; $i ++) {
+        if ($str [$i] == '%' && $str [$i + 1] == 'u') {
+            $val = hexdec ( substr ( $str, $i + 2, 4 ) );
+            if ($val < 0x7f)
+                $ret .= chr ( $val );
+            else if ($val < 0x800)
+                $ret .= chr ( 0xc0 | ($val >> 6) ) . chr ( 0x80 | ($val & 0x3f) );
+            else
+                $ret .= chr ( 0xe0 | ($val >> 12) ) . chr ( 0x80 | (($val >> 6) & 0x3f) ) . chr ( 0x80 | ($val & 0x3f) );
+            $i += 5;
+        } else if ($str [$i] == '%') {
+            $ret .= urldecode ( substr ( $str, $i, 3 ) );
+            $i += 2;
+        } else
+            $ret .= $str [$i];
+    }
+    return $ret;
+}
 if(isset($_SESSION['user']) == false || $_SESSION['type'] != '2'){
     header("location:/index.php");
 }
@@ -63,9 +84,9 @@ $pay = mysql_fetch_array($pay);
 	</script>
     <script type="text/javascript">
         function init() {
-            document.getElementById("pub_key").value = unescape("<?php echo $pay['pubKey']?>");
-            document.getElementById("pri_key").value = unescape("<?php echo $pay['priKey']?>");
-            document.getElementById("app_id").value = unescape("<?php echo $pay['appId']?>");
+            document.getElementById("pub_key").value = "<?php echo unescape($pay['pubKey']);?>";
+            document.getElementById("pri_key").value = "<?php echo unescape($pay['priKey']);?>";
+            document.getElementById("app_id").value = "<?php echo unescape($pay['appId']);?>";
         }
         function submitInfo() {
 
